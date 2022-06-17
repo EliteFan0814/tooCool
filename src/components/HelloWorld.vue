a2a4b83e29a85f9234f2dd7c733c99e188fe1157
fanpeichao <fan552426811@outlook.com>
2022-06-17 17:33:31 +0800

    add all

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>这是和</h1>
+  <div class="haha"><div class="ha">hehe</div></div>
+</template>
+
+<style lang="scss" scoped>
+.haha {
+  .ha {
+    color: red;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import '../node_modules/modern-css-reset/dist/reset.min.css'
+// import ''
+createApp(App).mount('#app')
